3117e08b4ec536a545f6fde52d7b01a3ac41dcd9
Florian <florian-1992@hotmail.fr>
2019-07-19 12:45:46 +0200

    Working on the secondary submenus after the user selects an option in the nav bar, everything's going well so far...

--- JS/adl.js
+++ JS/adl.js
@@ -1,5 +1,51 @@
 /* ==================== MAIN FUNCTIONS ==================== */
 
+function button_pressure_effect(submenu_button)
+{
+  submenu_button.style.boxShadow = "0px 5px 20px -10px rgba(0,0,0,0.57) inset";
+}
+
+//This function is used on arrows icon found in the right hand corner of
+//all submenus. It uses the display CSS attribute to hide elements with the
+//onclick event. The class name used is "collapsed" which can be found
+//in home_nav_bar.css under SUBMENU_BODY
+function expand_submenu(arrow_icon)
+{
+  let submenu_first_row = arrow_icon.parentNode.nextElementSibling.children[1];
+  let submenu_second_row = arrow_icon.parentNode.nextElementSibling.children[2];
+
+  //If the submenu is collapsed then, onclick, expand it
+  if (submenu_first_row.className == "submenu_row collapsed")
+  {
+    submenu_first_row.className = "submenu_row";
+    submenu_second_row.className = "submenu_row";
+    arrow_icon.src = "./Icons/icons8-collapse-arrow-24.png";
+    arrow_icon.alt = "Flèche pour déplier le menu";
+  }
+
+  //Otherwise, onclick, collapse it
+  else
+  {
+    submenu_first_row.className = "submenu_row collapsed";
+    submenu_second_row.className = "submenu_row collapsed";
+    arrow_icon.src = "./Icons/icons8-expand-arrow-24.png";
+    arrow_icon.alt = "Flèche pour replier le menu";
+  }
+}
+
+function highlight_chosen_button(submenu_button)
+{
+  submenu_button.style.borderRadius = "5px";
+  submenu_button.style.boxShadow = "0px 5px 40px -10px rgba(0,0,0,0.57)";
+  submenu_button.style.transition= "all 0.3s ease 0s";
+}
+
+function remove_highlight_effect(submenu_button)
+{
+    submenu_button.removeAttribute("style", "border-radius");
+    submenu_button.removeAttribute("style", "box-shadow");
+}
+
 function set_unavailable_infobulle(obj)
 {
 
--- JS/home_nav_bar.js
+++ JS/home_nav_bar.js
@@ -1,42 +1,173 @@
 /* ==================== MAIN FUNCTIONS ==================== */
 
-function expand_submenu(obj)
+function clean_slate(starting_node)
 {
-  let row1 = obj.parentNode.nextElementSibling.children[1];
-  let row2 = obj.parentNode.nextElementSibling.children[2];
-  //let collapsed = "submenu_row default_collapse";
-  //let expanded = "submenu_row";
-  //window.alert(row1);
-  //window.alert(row2);
-
-  if (row1.className == "submenu_row default_collapse")
+  while(starting_node.hasChildNodes())
   {
-    row1.className = "submenu_row";
-    row2.className = "submenu_row";
-    obj.src = "./Icons/icons8-collapse-arrow-24.png";
-    obj.alt = "Flèche pour déplier le menu";
-  }
-
-  else
-  {
-    row1.className = "submenu_row default_collapse";
-    row2.className = "submenu_row default_collapse";
-    obj.src = "./Icons/icons8-expand-arrow-24.png";
-    obj.alt = "Flèche pour replier le menu";
+    starting_node.removeChild(starting_node.firstChild);
   }
 }
 
-function highlight_chosen_button(obj)
+function full_clean_slate(starting_node)
 {
-  obj.style.borderRadius = "5px";
-  obj.style.boxShadow = "0px 5px 40px -10px rgba(0,0,0,0.57)";
-  obj.style.transition= "all 0.3s ease 0s";
+  while(starting_node.hasChildNodes())
+  {
+    starting_node.removeChild(starting_node.firstChild);
+  }
+  starting_node.parentNode.removeChild(starting_node);
 }
 
-function remove_highlight_effect(obj)
+function show_next_submenu(chosen_button)
 {
-    obj.removeAttribute("style", "border-radius");
-    obj.removeAttribute("style", "box-shadow");
+  let nav_bar = document.getElementsByTagName("nav")[0];
+  let newsfeed = document.getElementById("newsfeed");
+  let button_name = chosen_button.getElementsByTagName("figcaption")[0].innerText;
+
+  clean_slate(nav_bar);
+  full_clean_slate(newsfeed);
+
+  switch(button_name)
+  {
+
+    //Sciences
+    case "Formelles":
+      create_sciences_formelles_submenu();
+    break;
+
+    case "Naturelles":
+    break;
+
+    case "Humaines":
+    break;
+
+    case "Appliquées":
+    break;
+
+    //Vie quotidienne
+    case "Entretiens":
+    break;
+
+    case "Relations":
+    break;
+
+    case "Orientation":
+    break;
+
+    case "Entraide":
+    break;
+
+    //Arts
+    case "Littérature":
+    break;
+
+    case "Visuels":
+    break;
+
+    case "Spectacle/Son":
+    break;
+
+    case "Appliqués":
+    break;
+
+    default:
+    window.alert("Quelque-chose ne s'est pas passé comme prévu, veuillez contacter l'administrateur de ce site");
+    break;
+  }
 }
 
 /* ==================== SECUNDARY FUNCTIONS ==================== */
+
+function create_button(img_src, img_alt, caption)
+{
+  let button = document.createElement("figure");
+  let button_picture = document.createElement("img");
+  let button_caption_container = document.createElement("figcaption");
+  let button_caption = document.createTextNode(caption);
+
+  button_picture.src = img_src;
+  button_picture.alt = img_alt;
+  button_picture.className = "icon_loner";
+  button.className = "button";
+
+  button_caption_container.className = "simple_caption";
+
+  button.appendChild(button_picture);
+  button_caption_container.appendChild(button_caption);
+  button.appendChild(button_caption_container);
+
+  return button;
+}
+
+function create_submenu_grid(element_container, id, nb_of_rows, nb_of_columns, size_of_rows, size_of_columns)
+{
+  let new_submenu_container = document.createElement(element_container);
+  new_submenu_container.style.display = "grid";
+  new_submenu_container.id = id;
+
+  for(let i = 0; i < nb_of_columns; i++)
+  {
+    new_submenu_container.style.gridTemplateColumns += " " + size_of_columns;
+  }
+
+  for(let i = 0; i < nb_of_rows; i++)
+  {
+    new_submenu_container.style.gridTemplateRows += " " + size_of_rows;
+  }
+
+  return new_submenu_container;
+}
+
+function create_sciences_formelles_submenu()
+{
+  //Creating submenu container
+  let formal_sciences_submenu_container = create_submenu_grid("div", "formal_sciences_container", 1, 2, "300px", "50vw");
+  let home_storyboard = document.getElementById("storyboard");
+
+  document.getElementsByTagName("body")[0].insertBefore(formal_sciences_submenu_container, home_storyboard);
+
+  let formal_sciences_container = document.getElementById("formal_sciences_container");
+
+  //Creating the columns
+  formal_sciences_container.appendChild(document.createElement("div"));
+  formal_sciences_container.appendChild(document.createElement("div"));
+
+  let maths_column = formal_sciences_container.children[0];
+  let cs_column = formal_sciences_container.children[1];
+
+  //Adding Titles to columns
+  maths_column.appendChild(document.createElement("h2"));
+  cs_column.appendChild(document.createElement("h2"));
+
+  let maths_title = formal_sciences_container.children[0].children[0];
+  let cs_title = formal_sciences_container.children[1].children[0];
+
+  formal_sciences_container.children[0].children[0].appendChild(document.createTextNode("Mathématiques"));
+  formal_sciences_container.children[1].children[0].appendChild(document.createTextNode("Informatique"));
+
+  //Creating maths grid with buttons
+  let maths_submenu_grid = create_submenu_grid("div", "maths_submenu", 2, 4, "110px", "150px");
+
+  maths_column.appendChild(maths_submenu_grid);
+
+  let maths_submenu = maths_column.children[1];
+
+  let analysis_button = create_button("./Icons/icons8-minimum-value-512.png", "Analyse", "Analyse");
+  let arithmetic_button = create_button("./Icons/icons8-math-64.png", "Arithmétique", "Arithmétique");
+  let statistics_button = create_button("./Icons/icons8-statistics-64.png", "Statistique", "Statistique");
+  let probabilities_button = create_button("./Icons/icons8-dice-64.png", "Probabilités", "Probabilités");
+
+  let methodology_button = create_button("./Icons/icons8-checklist-64.png", "Méthodes", "Méthodes");
+  let geometry_button = create_button("./Icons/icons8-measurement-tool-64.png", "Géométrie", "Géométrie");
+  let algebra_button = create_button("./Icons/icons8-data-sheet-64.png", "Algèbre", "Algèbre");
+  let misc_button = create_button("./Icons/icons8-ask-question-64.png", "Autres", "Autres");
+
+  maths_submenu.appendChild(analysis_button);
+  maths_submenu.appendChild(arithmetic_button);
+  maths_submenu.appendChild(statistics_button);
+  maths_submenu.appendChild(probabilities_button);
+
+  maths_submenu.appendChild(methodology_button);
+  maths_submenu.appendChild(geometry_button);
+  maths_submenu.appendChild(algebra_button);
+  maths_submenu.appendChild(misc_button);
+}
